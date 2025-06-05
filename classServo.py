acf70e68aecedaac824490c90393d7ff76c1af63
dwynnychuk <dwynnychuk@gmail.com>
2025-06-05 03:27:19 +0100

    Servo implementation working on rPi

--- classServo.py
+++ classServo.py
@@ -1,12 +1,30 @@
+from adafruit_servokit import ServoKit
+
+servoHat = ServoKit(channels=16)
+
 class Servo:
     def __init__(self, id):
         self.id = id
+        self.minAngle = 0
+        self.maxAngle = 180
+        self.homeAngle = 90
         print(f"ID: {self.id}")
         
-    def rotate_servo(self, position):
-        print(position)
+    def _rotate_servo(self, position):
+        if position < self.minAngle:
+            print(f"Demanded Angle of {position} on Servo {self.id} Less than minimum!")
+        elif position > self.maxAngle:
+            print(f"Demanded Angle of {position} on Servo {self.id} Greater than maximum!")
+        else:
+            print(f"Moving Servo {self.id} to: {position}")
+            servoHat.servo[self.id].angle = position
     
+    def _reset_servo(self):
+        servoHat.servo[self.id].angle = self.homeAngle
+        print(f"Homed Servo {self.id} to {self.homeAngle}")
 
-servo = Servo(1)
 
-servo.rotate_servo(100)
+def home_position(servos: list) -> None:
+    for servo in servos:
+        servo._reset_servo()
+    
--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import classCamera
 import classControl
 import classRobot
 import classServo
+
+# mock servo implementation
+servo0 = classServo.Servo(0)
+servo1 = classServo.Servo(1)
+classServo.home_position([servo0, servo1])
+
 def main():
     cam = classCamera.Camera()
     
