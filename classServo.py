6433eb88be5a9e9570ba3239148a7004e6999758
dwynnychuk <dwynnychuk@gmail.com>
2025-06-04 19:14:23 -0600

    servo testing

--- classServo.py
+++ classServo.py
@@ -1,6 +1,12 @@
 class Servo:
     def __init__(self, id):
         self.id = id
+        print(f"ID: {self.id}")
         
     def rotate_servo(self, position):
-        pass
+        print(position)
+    
+
+servo = Servo(1)
+
+servo.rotate_servo(100)
