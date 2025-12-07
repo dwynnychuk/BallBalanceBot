6d473b39085ec4021b50134e008fdcfb823ac7a5
dwynnychuk <dwynnychuk@gmail.com>
2025-12-07 14:59:38 -0800

    Testing out main.py

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ import cv2 as cv
 logger = get_logger(__name__)
 
 def main():
-    servos = classServo.init_servos(3)
+    servos: classServo.Servo = classServo.init_servos(3)
     cam = classCamera.Camera()
     cam.start()
     try:
         while True:
             frame = cam.latest_frame
             if frame is not None:
-                cv.imshow("frame", cam.latest_frame)
+                #cv.imshow("frame", cam.latest_frame)
                 if cam.get_ball_position():
                     logger.debug(f"Ball POS: {cam.get_ball_position()}, TIME: {cam.delta_t}")
 
