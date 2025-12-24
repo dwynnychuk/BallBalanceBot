84fdd8be5cf6171318679fab5921d7bf1e0c681d
dwynnychuk <dwynnychuk@gmail.com>
2025-12-23 21:03:23 -0800

    classRobot inverse kinematics of second arm done

--- classRobot.py
+++ classRobot.py
@@ -40,12 +40,38 @@ class Robot:
         
         theta_1 = degrees(atan2(pj1z, pj1x-L[2]))
         
+        
         # Arm 02
         bj2x = (L[3]*nVec[2])/sqrt(4*nVec[2]**2 - (nVec[0] - sqrt(3)*nVec[1])**2)
         bj2y = -(sqrt(3)*L[3]*nVec[2])/sqrt(4*nVec[2]**2 - (nVec[0] - sqrt(3)*nVec[1])**2)
         bj2z = h - ((nVec[0] - sqrt(3)*nVec[1])*L[3])/sqrt((nVec[0] - sqrt(3)*nVec[1])**2 - 4*nVec[2]**2)
         
+        A_2 = (sqrt(3)*bj2y + 2*L[2] - bj2x)/bj2z
+        B_2 = (bj2x**2 + bj2y**2 + bj2z**2 + L[1]**2 - L[0]**2 - L[2]**2)/2*bj2z
+        C_2 = A_2**2 + 4
+        D_2 = 2*A_2*B_2 - 4*L[2]
+        E_2 = B_2**2 + L[2]**2 - L[1]**2
+        
+        pj2x = (-D_2 - sqrt(D_2**2 - 4*C_2*E_2))/(2*C_2)
+        pj2y = -sqrt(3)*pj2x
+        pj2z = sqrt(L[1]**2 - 4*pj2x**2 + 4*L[2]*pj2x - L[2]**2)
+        
+        theta_2 = degrees(atan2(pj2z, (sqrt(pj2x**2 + pj2y**2) - L[2])))
+    
+        
         # Arm 03
         bj3x = (L[3]*nVec[2])/sqrt(4*nVec[2]**2 - (nVec[0] + sqrt(3)*nVec[1])**2)
         bj3y = (sqrt(3)*L[3]*nVec[2])/sqrt(4*nVec[2]**2 - (nVec[0] + sqrt(3)*nVec[1])**2)
         bj3z = h - ((nVec[0] + sqrt(3)*nVec[1])*L[3])/sqrt((nVec[0] + sqrt(3)*nVec[1])**2 - 4*nVec[2]**2)
+
+        A_3 = 0
+        B_3 = 0
+        C_3 = 0
+        D_3 = 0
+        E_3 = 0
+        
+        pj3x = 0
+        pj3y = 0
+        pj3z = 0
+        
+        theta_3 = degrees(0)
