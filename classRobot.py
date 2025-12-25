4f201d417c0d19e7dc5a420a93e6b8e4b511625f
dwynnychuk <dwynnychuk@gmail.com>
2025-12-24 20:12:26 -0800

    inverse kinematics complete

--- classRobot.py
+++ classRobot.py
@@ -22,6 +22,7 @@ class Robot:
         bj: Ball joint
         """
         L = self.L
+        angle_offset = 90
         
         # reference position (theta 0)
         A = (L[1] + L[2])/h
@@ -39,7 +40,7 @@ class Robot:
         bj1z = h - (nVec[0]*L[3])/bj_denom_1
         
         A_1 = (L[2] - bj1x)/bj1z
-        B_1 = (bj1x**2 + bj1y**2 + bj1z**2 + L[1]**2 - L[0]**2 - L[2]**2) / 2*bj1z
+        B_1 = (bj1x**2 + bj1y**2 + bj1z**2 + L[1]**2 - L[0]**2 - L[2]**2) / (2*bj1z)
         C_1 = A_1**2 + 1
         D_1 = 2*(A_1*B_1 - L[2])
         E_1 = B_1**2 + L[2]**2 - L[1]**2
@@ -52,7 +53,8 @@ class Robot:
         if bj1z < PZ:
             pj1z = -pj1z
         
-        theta_1 = degrees(atan2(pj1z, pj1x-L[2]))
+        # adjust for physical build (0 -> vertical)
+        theta_1 = angle_offset - degrees(atan2(pj1z, pj1x-L[2]))
         
         
         # Arm 02
@@ -61,21 +63,22 @@ class Robot:
         bj2y = (sqrt(3)*L[3]*nVec[2])/bj_denom_2
         bj2z = h + ((nVec[0] - sqrt(3)*nVec[1])*L[3])/bj_denom_2
         
-        A_2 = (sqrt(3)*bj2y + 2*L[2] - bj2x)/bj2z
+        A_2 = (sqrt(3)*bj2y - 2*L[2] - bj2x)/bj2z
         B_2 = (bj2x**2 + bj2y**2 + bj2z**2 + L[1]**2 - L[0]**2 - L[2]**2)/2*bj2z
         C_2 = A_2**2 + 4
-        D_2 = 2*A_2*B_2 - 4*L[2]
+        D_2 = 2*(A_2*B_2 + 2*L[2])
         E_2 = B_2**2 + L[2]**2 - L[1]**2
         
         pj2x = (-D_2 - sqrt(D_2**2 - 4*C_2*E_2))/(2*C_2)
         pj2y = -sqrt(3)*pj2x
-        pj2z = sqrt(L[1]**2 - 4*pj2x**2 + 4*L[2]*pj2x - L[2]**2)
+        pj2z = sqrt(L[1]**2 - 4*pj2x**2 - 4*L[2]*pj2x - L[2]**2)
         
         # check elbow up vs elbow down
         if bj2z < PZ:
             pj2z = -pj2z
-        
-        theta_2 = degrees(atan2(pj2z, (sqrt(pj2x**2 + pj2y**2) - L[2])))
+            
+        # adjust for physical build (0 -> vertical)
+        theta_2 = angle_offset - degrees(atan2(pj2z, (sqrt(pj2x**2 + pj2y**2) - L[2])))
     
         
         # Arm 03
@@ -87,7 +90,7 @@ class Robot:
         A_3 = -(bj3x + sqrt(3)*bj3y + 2*L[2])/bj3z
         B_3 = (bj3x**2 + bj3y**2 + bj3z**2 + L[1]**2 - L[0]**2 - L[2]**2)/(2*bj3z)
         C_3 = A_3**2 + 4
-        D_3 = 2*A_3*B_3 + 4*L[2]
+        D_3 = 2*(A_3*B_3 + 2*L[2])
         E_3 = B_3**2 + L[2]**2 - L[1]**2
         
         pj3x = (-D_3 - sqrt(D_3**2 - 4*C_3*E_3))/(2*C_3)
@@ -97,5 +100,10 @@ class Robot:
         # check elbow up vs elbow down
         if bj3z < PZ:
             pj3z = -pj3z
+            
+        # adjust for physical build (0 -> vertical)
+        theta_3 = angle_offset - degrees(atan2(pj3z, (sqrt(pj3x**2 + pj3y**2) - L[2])))
+
+        thetas = [theta_1, theta_2, theta_3]
         
-        theta_3 = degrees(atan2(pj3z, (sqrt(pj3x**2 + pj3y**2) - L[2])))
+        return thetas
