a396855742b9558a1f65bb845b8c9a645079e4e1
dwynnychuk <dwynnychuk@gmail.com>
2026-01-02 02:27:27 +0000

    Camera calibration mods and calibration data with new camera for raspberry pi

--- cameraCalibration.py
+++ cameraCalibration.py
@@ -12,33 +12,77 @@ class Calibration:
         self.ncol = 6
         self.criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
         self.defaultCalNum = 20
+        self.camera_fov = (1280, 720)
+        
+        # see if running on raspberry pi
+        try:
+            from picamera2 import Picamera2
+            self.Pi_Camera_Available = True
+            self.picam2 = Picamera2()
+        except:
+            self.Pi_Camera_Available = False                
         
     def take_calibration_images(self):
         # Open camera
-        fp1 = os.getcwd() + "/calibration/"
-        cap = cv.VideoCapture(0)
-        if not cap.isOpened():
-            raise RuntimeError("Cannot open camera")
+        os.makedirs(self.fp, exist_ok=True)
+        
+        if self.Pi_Camera_Available:
+            config = self.picam2.create_video_configuration(
+                main={"size": self.camera_fov}
+                )
+            self.picam2.configure(config)
+            self.picam2.start()
+            get_frame = lambda: cv.cvtColor(
+                self.picam2.capture_array(),
+                cv.COLOR_RGB2BGR
+                )
+            print("Using Raspberry Pi Camera for Calibration")
+        else:
+            cap = cv.VideoCapture(0)
+            if not cap.isOpened():
+                raise RuntimeError("Cannot open camera")
+            get_frame = lambda: cap.read()[1]
+            print("Using Mac Camera for Calibration")
         
         time.sleep(1)
-        for i in range(self.defaultCalNum):
-            ret, frame = cap.read()
-            if not ret:
-                raise RuntimeError("Cannot access frame")
-            cv.imshow(f"{i}",frame)
-            cv.waitKey(100)
-            time.sleep(2)
-            cv.imwrite(fp1 + f"calibrationmac{i}.jpg",frame)
-            
-        cap.release()
-        cv.destroyAllWindows()
-    
+        
+        try: 
+            for i in range(self.defaultCalNum):
+                print(f"Capturing Image: {i}")
+                time.sleep(0.5)
+                frame = get_frame()
+                if frame is None:
+                    continue
+                
+                #cv.imshow(f"{i}",frame)
+                cv.waitKey(100)
+                
+                print(f"Captured image: {i}, saving...")
+                
+                cv.imwrite(self.fp + f"calibration_{i}.jpg",frame)
+                print(f"Saved image: {i}")
+                time.sleep(2)            
+                
+        finally:
+            if self.Pi_Camera_Available:
+                self.picam2.stop()
+            else:
+                cap.release()
+        
+            cv.destroyAllWindows()
+                
     def calibrate_camera(self, showAll = True, printAll = True):
         worldpointscur = np.zeros((self.nrow*self.ncol, 3), np.float32)
         worldpointscur[:,:2] = np.mgrid[0:self.nrow, 0:self.ncol].T.reshape(-1,2)
+
         worldpointlist = []
         imagepointlist = []
+        
         images = glob.glob(os.path.join(self.fp, "*.jpg"))
+        
+        if not images:
+            raise RuntimeError("No Calibration Images Found")
+        
         for imgPath in images:
             imgBGR = cv.imread(imgPath)
             imgGr = cv.cvtColor(imgBGR, cv.COLOR_BGR2GRAY)
@@ -61,6 +105,15 @@ class Calibration:
         print(f"Representation Error:\n{er}\n")
         print(f"Camera Matrix:\n{camMat}\n")
         print(f"Distortion Coefficients:\n{distCoefs}\n")
+        
+        if self.Pi_Camera_Available:
+            np.savez(
+                self.fp + "pi_camera_calibration.npz",
+                camera_matrix = camMat,
+                distortion_coefficients = distCoefs,
+                image_size = imgGr.shape[::-1]
+                )
+        
         if printAll:
             print(f"Rotation Vector:\n{rVec}\n")
             print(f"Translation Vector:\n{tVec}")
