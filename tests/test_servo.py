632eccbcd0b20dcaf764ff2cd14db23dbc19a9ad
dwynnychuk <dwynnychuk@gmail.com>
2026-08-16 16:24:46 -0700

    Add magic mock tests to servo tests

--- tests/test_servo.py
+++ tests/test_servo.py
@@ -1,4 +1,5 @@
 import pytest
+from unittest.mock import MagicMock
 from classServo import Servo, ServoHardwareError
 
 def test_servo_init_defaults():
@@ -39,7 +40,32 @@ def test_rotate_absolute_above_range():
     
     result = servo.rotate_absolute(-50)
     assert result == servo.MAX_ANGLE
-
+    
+def test_rotate_absolute_sets_kit_angle():
+    mock_kit = MagicMock()
+    servo = Servo(0, kit=mock_kit)
+    
+    result = servo.rotate_absolute(30)
+    
+    assert result == 60
+    assert mock_kit.servo[0].angle == 60
+    
+def test_reset():
+    mock_kit = MagicMock()
+    servo = Servo(0, kit=mock_kit, home_angle=45)
+    
+    servo.reset()
+    
+    assert mock_kit.servo[0].angle == 45
+    
 def test_servo_hardware_error():
     with pytest.raises(RuntimeError):
         raise ServoHardwareError()
+    
+def test_initalize_servo_range():
+    mock_hat = MagicMock()
+    
+    Servo._initialize_servo_range(mock_hat, 3, 500, 2500)
+    
+    mock_hat.servo[0].set_pulse_width_range.assert_called_with(500,2500)
+    mock_hat.servo[2].set_pulse_width_range.assert_called_with(500,2500)
